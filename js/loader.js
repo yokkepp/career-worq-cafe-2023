0e2e82f8f6f8a99d4acc6870d18b43eecb7108fb
Yousuke Serita <serita@star-career.co.jp>
2023-01-07 15:50:55 +0900

    add animation.

---
+++ js/loader.js
@@ -0,0 +1,13 @@
+$(function () {
+  var loader = $(".loader-wrap");
+
+  //ページの読み込みが完了したらアニメーションを非表示
+  $(window).on("load", function () {
+    loader.fadeOut();
+  });
+
+  //ページの読み込みが完了してなくても3秒後にアニメーションを非表示にする
+  setTimeout(function () {
+    loader.fadeOut();
+  }, 3000);
+});
